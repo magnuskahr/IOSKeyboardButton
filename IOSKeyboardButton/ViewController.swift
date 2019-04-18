d6d71616dbb9205544a0e138321da437517c9382
Magnus Jensen <magnuskahr@icloud.com>
2019-04-18 11:11:55 +0200

    Initial commit

--- IOSKeyboardButton/ViewController.swift
+++ IOSKeyboardButton/ViewController.swift
@@ -12,9 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(red: 0.82, green: 0.83, blue: 0.85, alpha: 1.00)
+        
+        let zero = makeButton(with: "0", and: NormalKeyButtonFormatter())
+        zero.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        view.addSubview(zero)
+        
+        let one = makeButton(with: "1", and: SpecielKeyButtonFormatter())
+        one.frame = CGRect(x: 100, y: 300, width: 100, height: 100)
+        view.addSubview(one)
+        
     }
 
+    private func makeButton(with title: CustomStringConvertible, and formatter: KeyboardButtonFormatter) -> KeyboardButton {
+        let button = KeyboardButton(title: title, formatter: formatter)
+        button.delegate = self
+        return button
+    }
 
 }
 
+extension ViewController: KeyboardButtonDelegate {
+    func keyWasHit(_ button: KeyboardButton) {
+        guard let title = button.titleLabel?.text else {
+            return
+        }
+        
+        let alert = UIAlertController(title: "Hit", message: "The key \(title) was hit", preferredStyle: .alert)
+        alert.addAction(.init(title: "Okay", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
